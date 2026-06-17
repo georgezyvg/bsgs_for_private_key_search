4dcbdbee56ede351c5ee2a64a761fa60c9d6361a
georgezyvg <georgeputta355@gmail.com>
2026-06-18 00:50:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src import KeyFinder
 
 if __name__ == "__main__":
     target_public_key = "02e0a8b039282faf6fe0fd769cfbc4b6b4cf8758ba68220eac420e32b91ddfa673"
-    target_address = "1HsMJxNiV7TLxmoF6uJNkydxPFDog4NQum"
+    target_address = "1NBC8uXJy1GiJ6drkiZa1WuKn51ps7EPTv"
     start_range_hex = "80000"
     end_range_hex = "fffff"
 
