a4219d28ec9eaae1960d5a5c92e8a6bb919ca1f0
georgezyvg <georgeputta355@gmail.com>
2026-06-18 00:53:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     target_public_key = "02e0a8b039282faf6fe0fd769cfbc4b6b4cf8758ba68220eac420e32b91ddfa673"
     target_address = "1NBC8uXJy1GiJ6drkiZa1WuKn51ps7EPTv"
     start_range_hex = "800000000000000000000000000000000000000"
-    end_range_hex = "fffff"
+    end_range_hex = "fffffffffffffffffffffffffffffffffffffff"
 
     KeyFinder().solve_puzzle(
         target_public_key, target_address, start_range_hex, end_range_hex
