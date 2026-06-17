f8070b7a8ea4cc1828ca2a0b83ace587f9c89a8a
georgezyvg <georgeputta355@gmail.com>
2026-06-18 00:52:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src import KeyFinder
 if __name__ == "__main__":
     target_public_key = "02e0a8b039282faf6fe0fd769cfbc4b6b4cf8758ba68220eac420e32b91ddfa673"
     target_address = "1NBC8uXJy1GiJ6drkiZa1WuKn51ps7EPTv"
-    start_range_hex = "80000"
+    start_range_hex = "800000000000000000000000000000000000000"
     end_range_hex = "fffff"
 
     KeyFinder().solve_puzzle(
